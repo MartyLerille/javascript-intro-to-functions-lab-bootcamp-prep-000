91c0017c2cb19efe04d1994266d521446d8276bb
Marty Lerille <mlerille@tulane.edu>
2018-07-26 16:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function shout(string){
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string){
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string){
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 if (string === string.toLowerCase()) {
   console.log("I can't hear you!");
 }
-else (string === string.toUpperCase()); {
+else (string === string.toUpperCase()) {
   console.log("YES INDEED!");
 }
 
